6b156d5f9b32f9cae3f6557b0cf885c705bad8f1
jashanpreet <jsdihu55@academic.rrc.ca>
2025-01-17 14:17:21 -0600

    Created the init method to initialize the class attributes.

--- library_item/library_item.py
+++ library_item/library_item.py
@@ -1,6 +1,47 @@
 """"
 Description: A class to manage LibraryItem objects.
 """
-__author__ = ""
-__version__ = ""
+__author__ = "Jashanpreet Sidhu"
+__version__ = "1.0.0"
+
+from genre.genre import Genre
+
+class LibraryItem:
+    """
+    LibraryItem class: Used to maintain Library's data.
+
+    """
+    def __init__(self, title: str, author: str, genre: Genre):
+        """
+        To initialize the class attributes with attribute values.
+
+        Args:
+            title (str): The title of the library item.
+            author (str): The author of the library item.
+            genre (Genre): The Genre of the library item.
+
+        Returns:
+            None
+
+        Raises:
+            ValueError: When title is blank, the author is blank or the genre is blank.
+        
+        """
+        if len(title.strip()) > 0:
+            self.__title = title
+
+        else:
+            raise ValueError("Title cannot be blank.")
+        
+        if len(author.strip()) > 0:
+            self.__author = author
+
+        else:
+            raise ValueError("Author cannot be blank.")
+        
+        if isinstance(genre, Genre):
+            self.__genre = genre
+        
+        else:
+            raise ValueError("Invalid Genre.")
 
